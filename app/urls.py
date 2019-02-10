e4172dc0bd18e38fde83b2c745cd04c06b5a7f62
aayush <aayushkothari11@yahoo.com>
2019-02-10 08:25:12 +0530

    static

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'app/dashboard.html', {})
