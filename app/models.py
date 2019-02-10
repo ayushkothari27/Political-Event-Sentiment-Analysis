eb294356317e44e3218c6731419eafa4a04913f8
aayush <aayushkothari11@yahoo.com>
2019-02-10 08:31:24 +0530

    event

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Twitter(models.Model):
+    event = models.CharField(max_length=200, blank=True, null=True)
     tweet_id = models.CharField(max_length=200, blank=True, null=True)
     score = models.CharField(max_length=200, blank=True, null=True)
 
