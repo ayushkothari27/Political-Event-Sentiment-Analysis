f2f52b99b4468473d94082d45d468fd393fd3c4c
aayush <aayushkothari11@yahoo.com>
2019-02-10 08:11:14 +0530

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Twitter(models.Model):
+    tweet_id = models.CharField(max_length=200, blank=True, null=True)
+    score = models.CharField(max_length=200, blank=True, null=True)
+
+class Quora(models.Model):
+    event = models.CharField(max_length=200, blank=True, null=True)
+    score = models.CharField(max_length=200, blank=True, null=True)
