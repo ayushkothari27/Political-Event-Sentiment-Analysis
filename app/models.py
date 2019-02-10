70590c124dfc293de37d5d0e4d73165b425df756
aayush <aayushkothari11@yahoo.com>
2019-02-10 11:31:58 +0530

    dashboard frontend

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Twitter(models.Model):
     event = models.CharField(max_length=200, blank=True, null=True)
     tweet_id = models.CharField(max_length=200, blank=True, null=True)
-    score = models.CharField(max_length=200, blank=True, null=True)
+    score_pos = models.CharField(max_length=200, blank=True, null=True)
+    score_neg = models.CharField(max_length=200, blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
 
 class Quora(models.Model):
@@ -18,6 +19,10 @@ class Quora(models.Model):
 
 class Event(models.Model):
     event_name = models.CharField(max_length=200, blank=True, null=True)
+    score = models.CharField(max_length=200, blank=True, null=True)
+    pos_score = models.CharField(max_length=200, blank=True, null=True)
+    neu_score = models.CharField(max_length=200, blank=True, null=True)
+    neg_score = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.event_name
